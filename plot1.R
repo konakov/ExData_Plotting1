ec330e64201de57934a439bf4f4dea7620c5a1d0
Sergei Konakov <konakov.s@gmail.com>
2014-05-06 21:54:56 +0400

    plot1 done

---
+++ plot1.R
@@ -0,0 +1,22 @@
+################
+## Load the data
+################
+
+## For fast loadind, first get the col classes, and then load the data.
+## Data file name is "household_power_consumption.txt"
+data_5rows <- read.table("household_power_consumption.txt", sep=";", header=T, nrow=5, na.strings = "?", stringsAsFactors=FALSE)
+classes <- sapply(data_5rows, class)
+data <- read.table("household_power_consumption.txt", sep=";", header=T, colClasses=classes, na.strings = "?", stringsAsFactors=FALSE)
+
+## convert the fist column to data for convience
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+## the project requires inly data for 2007-02-01 and 2007-02-02
+data <- subset(data, Date == as.Date("01/02/2007", "%d/%m/%Y") | Date == as.Date("02/02/2007", "%d/%m/%Y"))
+
+## plot the plot1
+png(filename = "plot1.png", width = 480, height = 480, units = "px", pointsize = 12, bg = "white")
+hist(data$Global_active_power, col="red", main="Global active power", xlab="Global active power (kilowatts)")
+dev.off()
+
+print("Plot 1 finished to plot1.png")
